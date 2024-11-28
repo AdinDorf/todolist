d66f03402c00ba78cb55823ecc6f4324bd15d1e1
AdinDorf <adin@dorffamily.com>
2024-11-27 21:33:01 -0600

    Created task.js

--- modules/task.js
+++ modules/task.js
@@ -1,3 +1,27 @@
-export function createTask(base) {
+export function createTask(baseNode) {
+    const prioDiv = document.createElement("div");
+    prioDiv.classList.add(prioDiv);
+
+    const prioInput = document.createElement("input");
+    prioInput.setAttribute("type", "number");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("id", "deleteButton");
+
+    const textDiv = document.createElement("div");
+    
+    const noteTitle = document.createElement("h1");
+    const noteContent = document.createElement("p");
+
+    const completeButton = document.createElement("button");
+    completeButton.setAttribute("id", "completeButton");
+
+    baseNode.appendChild(prioDiv);
+    prioDiv.appendChild(prioInput);
+    prioDiv.appendChild(deleteButton);
+    baseNode.appendChild(textDiv);
+    textDiv.appendChild(noteTitle);
+    textDiv.appendChild(noteContent);
+    baseNode.appendChild(completeButton);
 
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import "./styles.css"
 import { createNavBar } from "../modules/header";
+import { createTask } from "../modules/task";
+
 const content = document.getElementById("content");
 
-createNavBar(content);
+//createNavBar(content);
+createTask(content);
 
 
